bdb419f6bf324b9a2ddbcf27a8ed149cdba76245
dondakeshimo <went.went.takkun135@gmail.com>
2018-08-01 02:22:24 +0900

    [fix]ディレクトリを指定できるように変更

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,16 +2,17 @@ import glob
 import json
 import numpy as np
 from PIL import Image
+import sys
 
 
-def make_path_list(data_path="data/JLeaguers"):
-    img_list = glob.glob(data_path + "images/*.jpg")
+def make_path_list(data_path="data/"):
+    img_list = glob.glob(data_path + "JLeaguers/images/*.jpg")
     ant_list = [s.replace("images", "annotations") for s in img_list]
     ant_list = [s.replace("jpg", "json") for s in ant_list]
     return img_list, ant_list
 
 
-def load_data(img_list, ant_list):
+def load_data(img_list, ant_list, data_dir):
     img = []
     ant = []
     for img_path, ant_path in zip(img_list, ant_list):
@@ -23,13 +24,13 @@ def load_data(img_list, ant_list):
         ant.append(ant_dict["birthplace"])
 
     img = np.array(img)
-    ant = make_country_bin(ant)
+    ant = make_country_bin(ant, data_dir)
 
     return img, ant
 
 
-def make_country_bin(ant):
-    with open("data/prefecture.txt", "rt") as f:
+def make_country_bin(ant, data_dir):
+    with open(data_dir + "prefecture.txt", "rt") as f:
         prefecture = f.read().split("\n")
 
     ant_bin = [0 if i in prefecture else 1 for i in ant]
@@ -37,11 +38,12 @@ def make_country_bin(ant):
     return ant_bin.reshape(-1, 1)
 
 
-def main():
-    img_list, ant_list = make_path_list()
-    img, ant = load_data(img_list, ant_list)
+def main(data_dir):
+    img_list, ant_list = make_path_list(data_dir)
+    img, ant = load_data(img_list, ant_list, data_dir)
     return img, ant
 
 
 if __name__ == "__main__":
-    main()
+    data_dir = sys.argv[1]
+    main(data_dir)
